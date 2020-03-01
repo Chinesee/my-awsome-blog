cc352ff09d8795bac974bc2f30e1b562c42b1155
czc <czc12580520@gmail.com>
2020-03-01 17:48:43 +0800

    ✅ 添加依赖

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
 
   head: {
     htmlAttrs: { lang: 'cmn-Hans-CN' },
-    title: process.env.npm_package_name || '',
+    title: '令狐少侠的个人空间',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -40,6 +40,7 @@ export default {
   purgeCSS: { enabled: false },
 
   plugins: [
+    '~/plugins/neos',
   ],
 
   buildModules: [
