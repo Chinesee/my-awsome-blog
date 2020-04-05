cb3c7a8f830a2e3c3cebc928314f0186b15d970b
czc <czc12580520@gmail.com>
2020-04-05 17:11:42 +0800

    ✔️ 同步代码

--- pages/index.vue
+++ pages/index.vue
@@ -27,75 +27,40 @@
         >
       </div>
 
-      <div class="flex xs:flex-col sm:flex-row justify-center items-center select-none">
-        <div
-          v-for="({ comp, title, desc, link }, i) in logos"
-          :key="i"
-          class="mx-12 xs:mb-4 md:mb-0 flex items-center"
-        >
-          <component
-            :is="comp"
-            class="xs:w-8 sm:w-12 mr-4"
-          ></component>
-          <div>
-            <p class="mb-1 xs:text-sm sm:text-lg text-gray-600 font-bold ">
-              <a
-                target="_blank"
-                :href="link"
-              >{{ title }}</a>
-            </p>
-            <p class="w-40 text-xs text-gray-500">{{ desc }}</p>
-          </div>
-        </div>
-      </div>
+      <banner class="my-16"></banner>
+
+      <recent-articles :articles="filterArticles"></recent-articles>
     </div>
   </div>
 </template>
 
 <script>
 import head from '~/mixins/head'
-import LogoNuxt from '~/components/logo/LogoNuxt.vue'
-import LogoNetlify from '~/components/logo/LogoNetlify.vue'
-import LogoGitee from '~/components/logo/LogoGitee.vue'
-
-const logos = [
-  {
-    comp: 'LogoNuxt',
-    title: '由Nuxt提供技术支持',
-    desc: '本博客使用了 Nuxt 框架生成静态页面',
-    link: 'https://zh.nuxtjs.org/guide',
-  },
-  {
-    comp: 'LogoNetlify',
-    title: '部署于Netlify',
-    desc: '优秀的内容管理网站，能实现网页快速部署',
-    link: 'https://www.netlify.com/',
-  },
-  {
-    comp: 'LogoGitee',
-    title: '源码已开源',
-    desc: '所有代码已托管至码云仓库，欢迎交流学习',
-    link: 'https://gitee.com/chinesee/blog-nuxt',
-  },
-]
+import Banner from '~/components/home/Banner'
+import RecentArticles from '~/components/home/RecentArticles'
 
 export default {
-  components: { LogoNuxt, LogoNetlify, LogoGitee },
-
+  components: { Banner, RecentArticles },
   mixins: [head({ headTitle: '首页' })],
 
   asyncData() {
     const resolve = require.context('~/contents/', true, /\.md$/)
-    const blogs = resolve.keys().map(key => {
-      const { attributes, meta } = resolve(key)
-      return { attributes, meta }
+    const articles = resolve.keys().map(key => {
+      const { attributes: { title, description, time }, meta: { resourcePath } } = resolve(key)
+      const paths = resourcePath.split('\\')
+      const path = paths.slice(paths.indexOf(`${process.env.blogRoot}`) + 1).join('/').replace('.md', '')
+      const timestamp = new Date(time.replace(/[(年)|(月)|(日)]/g, '/')).getTime()
+      return { path, title, description, time, timestamp }
     })
-
-    return { blogs }
+    const filterArticles = articles
+      .sort((a, b) => {
+        return a.timestamp > b.timestamp ? -1 : 1
+      })
+      .slice(0, 4)
+    return { filterArticles }
   },
 
   data: () => ({
-    logos,
   }),
 }
 </script>
