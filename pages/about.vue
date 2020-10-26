d1c7337dcce2d2c763a63e8ae61a9c9abfe5779f
令狐少侠 <czc12580520@gmail.com>
2020-10-26 22:26:48 +0800

    commit

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="h-full flex flex-col justify-center items-center text-center">
-    <div class="mb-10">
+    <div
+      style="background: #eaf9fa;"
+      class="xs:w-24 xs:h-24 md:w-32 md:h-32 mb-10 p-6 flex justify-center items-center rounded-full"
+    >
       <img
         alt="avatar"
-        class="xs:w-16 md:w-24"
-        src="~/assets/images/logo.png"
+        class="xs:w-16 md:w-20"
+        src="~/assets/images/avatar.png"
       >
     </div>
     <p class="xs:text-lg md:text-2xl font-bold">
@@ -20,6 +23,13 @@
       Trying various programming languages <br>
       Keep moving
     </p>
+
+    <div
+      style="width: 10rem; height: 3rem; border-radius: 1.5rem; line-height: 3rem;"
+      class="mt-16 bg-gray-700 text-white rounded-lg cursor-pointer"
+    >
+      联系我
+    </div>
   </div>
 </template>
 
